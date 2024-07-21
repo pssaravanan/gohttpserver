07cc9863a328a701231126cba1f4956c03c80c75
Saravanan <saravanan.s@sigmoidanalytics.com>
2024-07-21 12:16:56 +0530

    removed unnecessary comments

--- cmd/run.go
+++ cmd/run.go
@@ -89,13 +89,4 @@ func init() {
 
 	runCmd.Flags().IntVar(&port, "port", 3000, "Port")
 	runCmd.Flags().StringVar(&path, "path", ".", "Path")
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// runCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
